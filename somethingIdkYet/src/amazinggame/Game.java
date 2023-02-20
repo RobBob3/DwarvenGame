4c638433994b2a358fa24b19c955edcacce2d337
RobBob3 <84109733+RobBob3@users.noreply.github.com>
2023-02-20 00:54:16 -0500

    Add files via upload

---
+++ somethingIdkYet/src/amazinggame/Game.java
@@ -0,0 +1,15 @@
+package amazinggame;
+
+public class Game {
+    public void intro() throws InterruptedException {
+        System.out.println("Welcome, adventurer...");
+        Thread.sleep(2000);
+        System.out.println("...to your doom!!!");
+        printDivider(25);
+    }
+    public void printDivider(int dividerLength) {
+        for (int i = 0; i < dividerLength; i++) {
+            System.out.print("-");
+        }
+    }
+}
---
+++ somethingIdkYet/src/amazinggame/Main.java
@@ -0,0 +1,21 @@
+package amazinggame;
+
+class Main {
+    public static void main(String[] args) throws InterruptedException {
+        Game game = new Game();
+        game.intro();
+    }
+}
+/*
+dungeon / cavern for player to explore
+player is a dwarf
+player mines ores to sell
+can sell and buy at a shop
+potions
+enemies would be typical fantasy enemies trolls goblins etc
+player has spells
+player is wizard
+exp -> level -> grow your beard -> more powerful
+player has limited inventory to hold ores
+
+ */
